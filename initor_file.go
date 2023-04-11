65227bf99685c3e387182bee3378afeebf3c3281
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-04-11 04:04:19 +0100

    Add file name append

--- initor_file.go
+++ initor_file.go
@@ -1,14 +1,35 @@
 package log
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"sync"
 )
 
+func fileAppendix(fileName string, num int) string {
+	if num == 0 {
+		return fileName
+	}
+
+	return fileName + "." + fmt.Sprint(num)
+}
+
 // Write logs to file
 func NewLoggerFile(fileName string) LogCB {
 	return func() (logger DoLog, closer Closer) {
-		file, err := os.Create(fileName)
+		adder := 1
+		
+		for {
+			_, err := os.Stat(fileAppendix(fileName, adder))
+			if errors.Is(err, os.ErrNotExist) {
+				break
+			}
+
+			adder++
+		}
+
+		file, err := os.Create(fileAppendix(fileName, adder))
 
 		if err != nil {
 			panic(err)
